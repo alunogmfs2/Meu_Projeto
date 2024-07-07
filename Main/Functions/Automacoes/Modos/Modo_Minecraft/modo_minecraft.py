92bf80f9ed600f46baec4d5917d100b4b874d72d
alunogmfs2 <134213951+alunogmfs2@users.noreply.github.com>
2024-07-07 14:22:31 +0200

    Consertando

--- Main/Functions/Automacoes/Modos/Modo_Minecraft/modo_minecraft.py
+++ Main/Functions/Automacoes/Modos/Modo_Minecraft/modo_minecraft.py
@@ -1,4 +1,22 @@
-from f_pg import *
+import pyautogui as pg
+
+pg.PAUSE = 0.4
+
+def abrir(nome_app):
+    pg.press('winleft')
+    pg.typewrite(nome_app)
+    enter()
+    pg.sleep(1)
+
+escrever = pg.typewrite
+
+def pesquisar(texto):
+    escrever(texto)
+    enter()
+    pg.sleep(1)
+
+def enter():
+    pg.press('enter')
 
 # Abre o Curseforge
 abrir('Curseforge')
--- Main/Functions/Automacoes/Modos/Modo_Modelagem3d/modo_modelagem3d.py
+++ Main/Functions/Automacoes/Modos/Modo_Modelagem3d/modo_modelagem3d.py
@@ -1,8 +1,25 @@
-from f_pg import *
+import pyautogui as pg
+
+pg.PAUSE = 0.4
+
+def abrir(nome_app):
+    pg.press('winleft')
+    pg.typewrite(nome_app)
+    enter()
+    pg.sleep(1)
+
+escrever = pg.typewrite
+
+def pesquisar(texto):
+    escrever(texto)
+    enter()
+    pg.sleep(1)
+
+def enter():
+    pg.press('enter')
 
 # Abre o Fusion
 abrir('Fusion')
-
 
 # Abre o Google Chrome
 abrir('Google Chrome')
